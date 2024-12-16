655cafb18909d57a68986f6d9c8bc3edd625739b
Mahnoor <muhammad31zayyan@gmail.com>
2024-12-17 00:45:11 +0500

    js work 2

---
+++ main.js
@@ -0,0 +1,17 @@
+ var todoArray = [];
+
+
+var task1 = prompt('tell your first task')
+console.log(todoArray.push(task1)); 
+var task2 = prompt('tell your second task')
+ console.log(todoArray.push(task2));
+
+ var task3 = prompt('tell your third task')
+
+ console.log(todoArray.push(task3));
+
+
+ console.log(todoArray.length); 
+
+
+ console.log("Your To-Do List:", todoArray);
